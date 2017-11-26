2f960a1888ce00fc15aa607fc878a6c10644a348
BuildTools <unconfigured@null.spigotmc.org>
2017-11-26 00:23:25 +0000

    Initial commit

---
+++ src/main/java/co/uk/odysseius/alma/Alma.java
@@ -0,0 +1,20 @@
+package co.uk.odysseius.alma;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Alma.MODID, version = Alma.VERSION)
+public class Alma
+{
+    public static final String MODID = "alma";
+    public static final String VERSION = "0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    }
+}
